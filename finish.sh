4debb3ee75f8db76cee0069185d23c2b0e6a4821
mihai14launcher <sbarceamihaita183@gmail.com>
2024-08-18 01:56:19 +0300

    Create finish.sh

---
+++ finish.sh
@@ -0,0 +1,6 @@
+echo 'Uppy its now installed in your system you can use for example:'
+echo 'uppy --help'
+sleep 2
+echo 'Or just wait 4 seconds and Uppy will run the Basic command uppy --help'
+sleep 3
+uppy --help
