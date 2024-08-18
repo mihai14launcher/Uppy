6bd4083c279821909d9ba5c481877a1645ea765b
mihai14launcher <sbarceamihaita183@gmail.com>
2024-08-18 03:24:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 sudo apt install python-is-python3 pip nano -y
+sudo apt install python3-pip -y
+apt-get install python3-venv -y
+apt install python3.11-venv -y
 chmod +x install.sh
+clear
 ./install.sh
